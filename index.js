67684b778439ab1cb722e263c874853b133b10e6
Andres <andresjml@gmail.com>
2021-04-07 09:18:51 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(arr, str){
+    let newArr = arr.filter(e => {
+        return e.toUpperCase() === str.toUpperCase();
+    });
+    return newArr;
+};
+
+function fuzzyMatch(arr, str) {
+    let newArr= arr.filter(e => {
+        return e.charAt(0).toUpperCase() === str.charAt(0).toUpperCase();
+    });
+    return newArr;
+};
+
+function matchName(arr, str){
+    let newArr=arr.filter(obj =>{
+        return obj.name === str;
+    });
+    return newArr;
+}
